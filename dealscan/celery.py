9f98817f403997fec8788f5a95e748961bface1d
style77 <stylek777@gmail.com>
2023-11-18 00:32:34 +0100

    feat: celery integration

---
+++ dealscan/celery.py
@@ -0,0 +1,18 @@
+import os
+import ssl
+
+from celery import Celery
+from django.apps import apps
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dealscan.settings")
+
+app = Celery(
+    "dealscan",
+    broker_use_ssl={"ssl_cert_reqs": ssl.CERT_NONE},
+    redis_backend_use_ssl={"ssl_cert_reqs": ssl.CERT_NONE},
+)
+
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.conf.enable_utc = True
+
+app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
