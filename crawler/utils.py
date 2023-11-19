db02aab8a1c3d856dba218a8ede2f0bc97eb7fec
style77 <stylek777@gmail.com>
2023-11-19 03:17:42 +0100

    chore: formatter functions

---
+++ crawler/utils.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+from typing import List
+
+from django.db.models import Q
+
+from crawler.formatters.formatter import FeedFormatter
+from crawler.models import Offer, Source
+
+
+class BaseLogger:
+    def write(self, msg, *args, **kwargs):
+        print(msg)
+
+
+def update_feeds(logger=BaseLogger()) -> List[Offer]:
+    sources = Source.objects.filter(Q(active=True))
+    logger.write(f"Processing queue with size of: {sources.count()} feeds")
+
+    all_results = []
+    for source in sources:
+        if (
+            source.last_polled
+            and source.last_polled + timedelta(seconds=source.interval)
+            >= datetime.now()
+        ):
+            logger.write(f'"{source.name}" is on cooldown.')
+            continue
+        all_results.append(crawl_feed(source))
+
+    return all_results
+
+
+def crawl_feed(source: Source):
+    formatter = FeedFormatter(source)
+    offers = formatter.get_offers()
+    return offers
