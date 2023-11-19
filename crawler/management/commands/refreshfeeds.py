3f41b8e05bbff9381e764802cd946e35b0842523
style77 <stylek777@gmail.com>
2023-11-19 03:18:12 +0100

    feat: refreshfeeds command

--- crawler/management/commands/refreshfeeds.py
+++ crawler/management/commands/refreshfeeds.py
@@ -2,9 +2,18 @@ from typing import Any
 
 from django.core.management.base import BaseCommand
 
+from crawler.utils import update_feeds
+from crawler.models import Offer, OfferMetadata
+
 
 class Command(BaseCommand):
     help = "Refresh all RSS feeds and update database with them"
 
     def handle(self, *args: Any, **options: Any):
-        self.stdout.write(self.style.SUCCESS("Success"))
+        results = update_feeds(self.stdout)
+        for result in results:
+            _ = OfferMetadata.objects.bulk_create([offer.metadata for offer in result])
+            inserted_offers = Offer.objects.bulk_create(result)
+            self.stdout.write(
+                f"Success. Saved {len(inserted_offers)}/{len(result)} offers to database.", self.style.SUCCESS
+            )
