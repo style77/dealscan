4391cf1e29381be4bc0bf7b40ba4599d9d92a954
style77 <stylek777@gmail.com>
2023-11-21 00:13:51 +0100

    chore: add namespace to config endpoint

--- billing/urls.py
+++ billing/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from billing import views
 
 urlpatterns = [
-    path("config/", views.stripe_config),
+    path("config/", views.stripe_config, name="config"),
     path("checkout/", views.create_checkout_session, name="checkout"),
     path("plans/", views.plans, name="plans")
 ]
