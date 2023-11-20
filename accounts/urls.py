bb85525dc4d25b046d4a6582dc7d3539080284bd
style77 <stylek777@gmail.com>
2023-11-20 02:27:46 +0100

    refactor: change password reset url name

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ from accounts.views import MyLoginView, MySignupView, MyPasswordResetView, MyPas
 urlpatterns = [
     path(r'login/', MyLoginView.as_view(), name='account_login'),
     path(r'signup/', MySignupView.as_view(), name='account_signup'),
-    path(r'password_reset/', MyPasswordResetView.as_view(), name='account_reset_password'),
-    path(r'password_reset_done/', MyPasswordResetDoneView.as_view(), name='account_reset_password_done'),
+    path(r'password/reset/', MyPasswordResetView.as_view(), name='account_reset_password'),
+    path(r'password/reset/done/', MyPasswordResetDoneView.as_view(), name='account_reset_password_done'),
 ]
