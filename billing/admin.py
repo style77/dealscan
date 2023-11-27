28d1adf75da03037bb25a34f4d689f8122ccba6c
style77 <stylek777@gmail.com>
2023-11-27 02:00:22 +0100

    refactor: remove billing app, beacause of djstripe migration

--- billing/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from unfold.admin import ModelAdmin
-
-from billing.models import StripeUser
-
-
-@admin.register(StripeUser)
-class StripeUserAdmin(ModelAdmin):
-    ...
