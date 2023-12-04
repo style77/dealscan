84d0b364ee8abf1396e606289e0c373316622ac7
style77 <stylek777@gmail.com>
2023-12-04 23:04:42 +0100

    feat: offers view

--- crawler/urls.py
+++ crawler/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from crawler.views import billing_view, dashboard_view
+from crawler.views import billing_view, dashboard_view, offers_view, all_offers_view
 
 urlpatterns = [
     path("dashboard/", dashboard_view, name="dashboard"),
     path("dashboard/billing/", billing_view, name="dashboard_billing"),
+    path("dashboard/offers/", offers_view, name="dashboard_offers"),
+    path("dashboard/offers/all/", all_offers_view, name="dashboard_all_offers"),
 ]
--- crawler/views.py
+++ crawler/views.py
@@ -1,5 +1,6 @@
 import random
 from typing import Any, Dict, Optional
+from django import http
 
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
@@ -9,13 +10,39 @@ from djstripe.models import Customer
 from django.contrib.auth import get_user_model
 from django.db.models import Subquery
 
+from django.conf import settings
+from crawler.models import Offer
+
 from polls.models import Poll, PollAnswer
+from djstripe.models import Product
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import QueryDict
 
 LOGIN_URL = reverse_lazy("account_login")
 User = get_user_model()
 
 
-class DashboardView(TemplateView):
+class BaseTemplateView(TemplateView):
+    def _find_poll(self, user: User) -> Optional[Poll]:
+        answered_polls = PollAnswer.objects.filter(user=user).values("poll_id")
+        unanswered_polls = Poll.objects.exclude(id__in=Subquery(answered_polls))
+        if not unanswered_polls:
+            return None
+
+        return random.choice(unanswered_polls)
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        poll = self._find_poll(self.request.user)
+
+        if poll:
+            context.update({"poll_id": poll.id})
+
+        return context
+
+
+class DashboardView(BaseTemplateView):
     template_name = "dashboard.html"
 
     def _find_poll(self, user: User) -> Optional[Poll]:
@@ -31,9 +58,52 @@ class DashboardView(TemplateView):
         poll = self._find_poll(self.request.user)
 
         if poll:
-            context.update({
-                "poll_id": poll.id
-            })
+            context.update({"poll_id": poll.id})
+
+        return context
+
+
+class OffersView(BaseTemplateView):
+    template_name = "offers.html"
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class OfferComponentView(TemplateView):
+    template_name = "all_offers.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        base_queryset = Offer.objects.all()
+
+        filter_params = self.request.GET
+        filtered_offers = base_queryset
+
+        all_filter_params = ['model__make', 'model']
+        for param in all_filter_params:
+            if param in filter_params:
+                filtered_offers = filtered_offers.filter(**{param: filter_params[param]})
+
+        limit = filter_params.get('limit', 40)
+        if limit < 10:
+            limit = 10
+        elif limit > 150:
+            limit = 150
+
+        paginator = Paginator(filtered_offers.order_by("-publication_date"), limit)
+
+        page = self.request.GET.get('page')
+        try:
+            offers = paginator.page(page)
+        except PageNotAnInteger:
+            offers = paginator.page(1)
+        except EmptyPage:
+            offers = paginator.page(paginator.num_pages)
+
+        context["offers"] = offers
 
         return context
 
@@ -47,9 +117,18 @@ class BillingView(TemplateView):
         z = Customer.objects.filter(subscriber=self.request.user).first()
         subscriptions = z.subscriptions if z else None
         context["subscriptions"] = subscriptions
+        context["publishable_key"] = settings.STRIPE_PUBLISHABLE_KEY
+        context["products"] = Product.objects.filter(active=True)
 
         return context
+
+    def get(
+        self, request: http.HttpRequest, *args: Any, **kwargs: Any
+    ) -> http.HttpResponse:
+        return super().get(request, *args, **kwargs)
 
 
 dashboard_view = login_required(DashboardView.as_view(), login_url=LOGIN_URL)
 billing_view = login_required(BillingView.as_view(), login_url=LOGIN_URL)
+offers_view = login_required(OffersView.as_view(), login_url=LOGIN_URL)
+all_offers_view = login_required(OfferComponentView.as_view(), login_url=LOGIN_URL)
