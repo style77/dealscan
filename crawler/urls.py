add44500d4cd3dd1e103287cc7a2c6fa8a908557
style77 <stylek777@gmail.com>
2023-12-15 00:07:01 +0100

    fix: add djstripe webhook url

--- crawler/urls.py
+++ crawler/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 
 from crawler.views import all_offers_view, billing_view, dashboard_view, offers_view
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path("dashboard/billing/", billing_view, name="dashboard_billing"),
     path("dashboard/offers/", offers_view, name="dashboard_offers"),
     path("dashboard/offers/all/", all_offers_view, name="dashboard_all_offers"),
+    path("payments/", include("djstripe.urls", namespace="djstripe")),
 ]
