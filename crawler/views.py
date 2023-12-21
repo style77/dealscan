fd26e5586ad1174cef85d7a93198e5d435e316e4
style77 <stylek777@gmail.com>
2023-12-21 19:00:51 +0100

    chore: filter functions

--- crawler/views.py
+++ crawler/views.py
@@ -1,5 +1,6 @@
 import random
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, List, Tuple
+from django_stubs_ext import ValuesQuerySet
 
 import stripe
 from django import http
@@ -14,7 +15,9 @@ from djstripe import models
 from djstripe import settings as djstripe_settings
 from djstripe.models import Product
 
-from crawler.models import Offer
+from collections import namedtuple
+
+from crawler.models import Offer, OfferMetadata
 from polls.models import Poll, PollAnswer
 
 LOGIN_URL = reverse_lazy("account_login")
@@ -71,13 +74,125 @@ class DashboardView(BaseTemplateView):
 class OffersView(BaseTemplateView):
     template_name = "offers.html"
 
+    def clear_params(self, params: List[str]) -> List[str]:
+        return [param for param in params if param != ""]
+
     def get_context_data(self, **kwargs) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
+
+        current_selected_makes = self.clear_params(self.request.GET.getlist("makes"))
+        context["current_selected_makes"] = current_selected_makes
+        current_selected_models = self.clear_params(self.request.GET.getlist("models"))
+        context["current_selected_models"] = current_selected_models
+
+        current_selected_years = self.clear_params(self.request.GET.getlist("years"))
+        context["current_selected_years"] = current_selected_years
+        current_selected_colors = self.clear_params(self.request.GET.getlist("colors"))
+        context["current_selected_colors"] = current_selected_colors
+
+        context["makes"] = self.__get_make_data()
+        context["models"] = self.__get_model_data(current_selected_makes)
+        context["years"] = self.__get_production_year(current_selected_makes, current_selected_models)
+        context["colors"] = self.__get_colors(current_selected_makes, current_selected_models)
+        context["transmissions"] = Offer.objects.values_list("transmission")
+
+        context["sources"] = Offer.objects.values_list("source")
+
+        context["forward_params"] = self.request.GET.urlencode()
+
         return context
+
+    def __get_make_data(self) -> List[namedtuple]:
+        Make = namedtuple("Make", ["name"])
+        makes = Offer.objects.values_list("model__make__name", flat=True).distinct()
+        make_objects = [Make(name=make) for make in makes]
+        return sorted(make_objects, key=lambda x: x.name)
+
+    def __get_model_data(
+        self, current_selected_makes: Optional[List[str]]
+    ) -> List[namedtuple]:
+        Model = namedtuple("Model", ["name"])
+        models = (
+            Offer.objects.filter(model__make__name__in=current_selected_makes)
+            .values_list("model__name", flat=True)
+            .distinct()
+            if current_selected_makes
+            else Offer.objects.values_list("model__name", flat=True).distinct()
+        )
+        model_objects = [Model(name=model) for model in models]
+        return sorted(model_objects, key=lambda x: x.name)
+
+    def __get_production_year(
+        self,
+        current_selected_makes: Optional[List[str]],
+        current_selected_model: Optional[List[str]],
+    ) -> List[namedtuple]:
+        ProductionYear = namedtuple("ProductionYear", ["year"])
+        production_years = (
+            Offer.objects.filter(
+                model__make__name__in=current_selected_makes,
+                model__name__in=current_selected_model,
+            )
+            .values_list("production_year", flat=True)
+            .distinct()
+            if current_selected_makes and current_selected_model
+            else Offer.objects.filter(
+                model__make__name__in=current_selected_makes
+            )
+            .values_list("production_year", flat=True)
+            .distinct()
+            if current_selected_makes
+            else Offer.objects.filter(model__name__in=current_selected_model)
+            .values_list("production_year", flat=True)
+            .distinct()
+            if current_selected_model
+            else Offer.objects.values_list("production_year", flat=True).distinct()
+        )
+        production_year_objects = [
+            ProductionYear(year=production_year) for production_year in production_years
+        ]
+        return sorted(production_year_objects, key=lambda x: x.year, reverse=True)
+
+    def __get_colors(
+        self,
+        current_selected_makes: Optional[List[str]],
+        current_selected_model: Optional[List[str]],
+    ) -> List[namedtuple]:
+        Color = namedtuple("Color", ["color"])
+        colors = (
+            Offer.objects.filter(
+                model__make__name__in=current_selected_makes,
+                model__name__in=current_selected_model,
+            )
+            .values_list("metadata__color", flat=True)
+            .distinct()
+            if current_selected_makes and current_selected_model
+            else Offer.objects.filter(
+                model__make__name__in=current_selected_makes
+            )
+            .values_list("metadata__color", flat=True)
+            .distinct()
+            if current_selected_makes
+            else Offer.objects.filter(model__name__in=current_selected_model)
+            .values_list("metadata__color", flat=True)
+            .distinct()
+            if current_selected_model
+            else Offer.objects.values_list("metadata__color", flat=True).distinct()
+        )
+
+        get_friendly_color = lambda color: [value for k, value in OfferMetadata.COLOR_CHOICES if k == color][0]
+
+        color_objects = [Color(color=get_friendly_color(color)) for color in colors]
+        return sorted(color_objects, key=lambda x: x.color)
 
 
 class OfferComponentView(TemplateView):
     template_name = "all_offers.html"
+
+    def __filter_data(self, offers: Any, filter: Dict[str, Any]):  # todo change Any to BaseManager[Offer]
+        if filter["value"] and filter["value"] != [""] and filter["value"] != ["Clear"]:
+            offers = offers.filter(**{filter["key"]: filter["value"]})
+        return offers
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -87,12 +202,29 @@ class OfferComponentView(TemplateView):
         filter_params = self.request.GET
         filtered_offers = base_queryset
 
-        all_filter_params = ["model__make", "model"]
-        for param in all_filter_params:
-            if param in filter_params:
-                filtered_offers = filtered_offers.filter(
-                    **{param: filter_params[param]}
-                )
+        get_unfriendly_color = lambda color: [k for k, value in OfferMetadata.COLOR_CHOICES if value == color][0]
+
+        filters = {
+            "makes": {
+                "key": "model__make__name__in",
+                "value": filter_params.getlist("makes"),
+            },
+            "models": {
+                "key": "model__name__in",
+                "value": filter_params.getlist("models"),
+            },
+            "years": {
+                "key": "production_year__in",
+                "value": filter_params.getlist("years"),
+            },
+            "colors": {
+                "key": "metadata__color__in",
+                "value": [get_unfriendly_color(c) for c in filter_params.getlist("colors")],
+            },
+        }
+
+        for key in filter_params.keys():
+            filtered_offers = self.__filter_data(filtered_offers, filters.get(key))
 
         limit = filter_params.get("limit", 40)
         if limit < 10:
