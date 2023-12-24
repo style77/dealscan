10f0c9149ba4e2316d24d02d10a4f518b65fcffc
style77 <stylek777@gmail.com>
2023-12-24 15:44:49 +0100

    fix: allow demo user to login

--- dealscan/middleware.py
+++ dealscan/middleware.py
@@ -1,12 +1,19 @@
 from django.http import HttpResponseForbidden
 from django.utils.translation import gettext_lazy as _
 
+EXEMPT_PATHS = [
+    "/accounts/login/",
+]
+
 
 class DemoModeMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
-            return HttpResponseForbidden(_("This action is not allowed in demo mode."))
+        if request.method in ["POST", "PUT", "PATCH", "DELETE"]:
+            if request.path not in EXEMPT_PATHS:
+                return HttpResponseForbidden(
+                    _("This action is not allowed in demo mode.")
+                )
         return self.get_response(request)
